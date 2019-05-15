159f84f6031e64eeb78b6540b34632a5c62b8bbc
AntoineLepeltier <49394486+AntoineLepeltier@users.noreply.github.com>
2019-05-15 14:20:12 +0200

    test team explorer

--- Library/repos/Date/cPeriod.cpp
+++ Library/repos/Date/cPeriod.cpp
@@ -5,8 +5,8 @@ cPeriod::cPeriod() :cDate(0, 0, 0), _monthConvention(30), _yearConvention(360) {
 cPeriod::cPeriod(const int& day, 
 	             const int& month,
 	             const int& year,
-	             const int& monthConvention,
-	             const int& yearConvention)
+	             const int& monthConvention, //eg 30
+	             const int& yearConvention) // eg 360
 	: cDate(day, month, year), _monthConvention(monthConvention), 
 	  _yearConvention(yearConvention) {}
 
